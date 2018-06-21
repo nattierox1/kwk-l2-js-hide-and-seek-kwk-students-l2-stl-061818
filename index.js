cc158dc700542164f0df7c15235a64e6db19412c
nattierox1 <nattierox1@gmail.com>
2018-06-21 21:10:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 //Code your solution here
+function getFirstSelector(selector)
+{
+  return document.querySelector(selector)
+}
+
+function nestedTarget(){
+  return document.getElementById('nested').querySelector('div.target')
+}
+
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+}
+
+function increaseRankBy(n){
+  const lis = document.querySelectorAll('ul.ranked-list')
+  for (let i = 0; i < lis.length; i++){
+  lis[i].innerHTML = Number.parseInt((i + 1).toString())
+  }
+}
+function deepestChild() {
+  return document.querySelector('#grand-node div div div div')
+  
+}
